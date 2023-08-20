ed8f41b3d7c198aec7140ebf7edc6e6c02f09f14
mikolyants1 <y.mikolyants@mail.ru>
2023-08-20 10:28:42 +0300

    Update Todo.tsx

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import {Component,createRef,RefObject,ReactNode,ChangeEvent} from 'react';
+import {Component,createRef,ReactNode,ChangeEvent,RefObject} from 'react';
 import svg from '../red16.jpg'
 import { prop,style2 } from '../props/state';
 interface web{
@@ -7,9 +7,9 @@ interface web{
     value1?:string,
     con?:number,
     style?:{
-        transform:string,
-        transitionDuration:string,
-        transitionTimingFunction:string
+      transform:string,
+      transitionDuration:string,
+      transitionTimingFunction:string
     },
     backgroundColor?:string,
     height?:number,
@@ -17,7 +17,7 @@ interface web{
     progMass?:number[],
    }
 export default class Todo extends Component<prop,web>{
-       public items:Array<string>=[]
+       readonly items:Array<string>=[]
         state={
             text:this.items,
             value:'',
@@ -31,53 +31,58 @@ export default class Todo extends Component<prop,web>{
       height:200,
       progValue:1,
       progMass:[],
-}
+       }
 
-  public ref=createRef<HTMLInputElement>()
-  public wrap=createRef<HTMLDivElement>()
+    readonly ref=createRef<HTMLInputElement>()
+    readonly wrap=createRef<HTMLDivElement>()
     set=(x:number):void=>{
-     const img:NodeListOf<HTMLImageElement>=document.querySelectorAll('.img')
-      img[x>0?x-1:-x-1].style.transform=`translateX(${x>0?0:50}px)`
+    const img:NodeListOf<HTMLImageElement>=document.querySelectorAll('.img')
+    img[x>0?x-1:-x-1].style.transform=`translateX(${x>0?0:50}px)`
     }
     clear():void{
-    if (this.wrap.current) this.wrap.current.style.height='200px'
+    const {current}:RefObject<HTMLDivElement|null>=this.wrap
+    if (current) current.style.height='200px'
     this.items.length=0
     this.setState({text:this.items,con:0,height:200,
     progMass:[1],progValue:1})
     }
     press1=(x:number):void=>{
-    const {height,value,progMass}:web=this.state
+    const {height,value,progMass,con}:web=this.state
+    const {current}:RefObject<HTMLDivElement|null>=this.wrap
     switch (x) {
     case 0:
      if (value!=='') {
      progMass.length=0
      this.state.progValue=0
      const val:number=height<400?height+50:height
-     if (this.wrap.current) this.wrap.current.style.height=`${val}px`
+     if (current) current.style.height=`${val}px`
      this.items.push(value)
      this.setState({text:this.items,con:this.state.con+1,
      height:val,progMass:progMass,progValue:this.state.progValue})
      this.ref.current?.focus()
      }
-     break;
-    case 1:
+    break;
+      case 1:
      const val:string=value.trim().toLowerCase()
      const text:string[]=this.items.filter((item:string):Boolean=>item.toLowerCase().indexOf(val)!==-1)  
-     this.setState({text:text,con:text.length}) 
+     const val1:number=text.length<5?50*text.length+200:400
+     if (current) current.style.height=`${val1}px`
+     this.setState({text:text,height:val1}) 
       break;
-        default:
-        break;
+      default:
+      break;
       }  
     }
 
     delete=(x:number):void=>{
-    const {con,height,progMass}:web=this.state
+    const {con,progMass,progValue}:web=this.state
+    const {current}:RefObject<HTMLDivElement|null>=this.wrap
     progMass.push(con)
-    const val:number=con<5?height-50:height
-    if (this.wrap.current) this.wrap.current.style.height=`${val}px`
+    const val:number=con<5?con*50+200-50:400
+    if (current) current.style.height=`${val}px`
     this.items.splice(x,1)
     this.setState({text:this.items,con:con-1,height:val,
-      progMass:progMass,progValue:this.state.progValue+1})
+    progMass:progMass,progValue:progValue+1})
     }
     focus=(x:number):void=>{
     this.setState({backgroundColor:x==0?'rgb(210, 210, 210) ':''})
@@ -98,7 +103,7 @@ export default class Todo extends Component<prop,web>{
           onMouseOut={():void=>this.set(-(i+1))}>
          <div className='div' style={style1}>{item}</div>
          <img className='img' style={style}
-          onClick={():void=>this.delete(i)}  src={svg} />
+          onClick={():void=>this.delete(i)} src={svg} />
             </div>
     ))
      return <div>
